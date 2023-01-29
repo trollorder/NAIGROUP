0016ab740a89f734fe6c5506068914f63cb543a7
trollorder <73745120+trollorder@users.noreply.github.com>
2023-01-29 23:02:12 +0800

    logs update

--- pose_estimation_demo/Python Integration Code.py
+++ pose_estimation_demo/Python Integration Code.py
@@ -1,39 +1,31 @@
 from pathlib import Path
 
-from peekingduck.pipeline.nodes.dabble import fps
+from peekingduck.pipeline.nodes.dabble import fps , tracking ,statistics,bbox_count
 from peekingduck.pipeline.nodes.draw import bbox, legend
 from peekingduck.pipeline.nodes.input import visual
-from peekingduck.pipeline.nodes.model import yolo
+from peekingduck.pipeline.nodes.model import yolo , efficientdet
 from peekingduck.pipeline.nodes.output import media_writer, screen,csv_writer
 from peekingduck.runner import Runner
 from src.custom_nodes.dabble import debug
 
 def main():
     debug_node = debug.Node(pkd_base_dir=Path.cwd() / "src" / "custom_nodes")
-
     visual_node = visual.Node(source=str(Path.cwd() / "Video Of People Walking.mp4"))
-    yolo_node = yolo.Node(detect=["person"])
-    bbox_node = bbox.Node(show_labels=True)
-
-    fps_node = fps.Node()
-    legend_node = legend.Node(show=["fps"])
-    screen_node = screen.Node()
-
-    csv_writer_node = csv_writer.Node(file_path=str(Path.cwd() / "output" / "new.csv"))
-
+    effdetect_node = efficientdet.Node(detect=["person"])
+    tracking_node = tracking.Node(type = "iou")
+    statistics_node = statistics.Node(maximum = "ids")
+    bbox_count_node = bbox_count.Node()
+    csv_writer_node = csv_writer.Node(file_path=str(Path.cwd() / "output" / "new.csv"),stats_to_track= ['cum_max','count'])
     runner = Runner(
-        nodes=[
-            visual_node,
-            yolo_node,
+        nodes=[visual_node,
+            effdetect_node,
             debug_node,
-            bbox_node,
-            fps_node,
-            legend_node,
-            screen_node,
-            csv_writer_node,
-        ]
+            tracking_node,
+            statistics_node,
+            bbox_count_node,
+            csv_writer_node]
+        
     )
-    print(runner)
     runner.run()
 
 main()
