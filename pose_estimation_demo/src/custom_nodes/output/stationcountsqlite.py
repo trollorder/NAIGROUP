a32558225d72305df90c43ebeeff3d99c0fa06fa
trollorder <73745120+trollorder@users.noreply.github.com>
2023-02-03 16:47:37 +0800

    With train and station detection

--- pose_estimation_demo/src/custom_nodes/output/stationcountsqlite.py
+++ pose_estimation_demo/src/custom_nodes/output/stationcountsqlite.py
@@ -7,10 +7,10 @@ from datetime import datetime
 from peekingduck.pipeline.nodes.abstract_node import AbstractNode
 import sqlite3
 
-DB_FILE = "count.db"           # name of database file
+DB_FILE = "stationcount.db"           # name of database file
 
 class Node(AbstractNode):
-   """Custom node to save unique persons which is a list of unqiue id on screen and count on screen
+   """ Stationsql node to save unique persons which is a list of unqiue id on screen and count on screen. Exclusive of train count
 
    Args:
       config (:obj:`Dict[str, Any]` | :obj:`None`): Node configuration.
@@ -25,27 +25,27 @@ class Node(AbstractNode):
          # will create DB_FILE if it does not exist
          self.conn = sqlite3.connect(DB_FILE)
          self.logger.info(f"Connected to {DB_FILE}")
-         sql = """ CREATE TABLE IF NOT EXISTS counttable (
+         sql = """ CREATE TABLE IF NOT EXISTS stationcounttable (
                         datetime text,
                         uniqueperson text,
-                        count text
+                        stationcount text
                    ); """
          cur = self.conn.cursor()
          cur.execute(sql)
       except sqlite3.Error as e:
          self.logger.info(f"SQL Error: {e}")
 
-   def update_db(self, obj_attrs: str, count: int) -> None:
+   def update_db(self, obj_attrs: str, stationcount: int) -> None:
       """Helper function to save current time stamp, hand direction and
       wave count into DB wavetable.
       """
       uniqueperson = str(obj_attrs) #manual coversion due to inability to rename
       now = datetime.now()
       dt_str = f"{now:%Y-%m-%d %H:%M:%S}"
-      sql = """ INSERT INTO counttable(datetime,uniqueperson,count)
+      sql = """ INSERT INTO stationcounttable(datetime,uniqueperson,stationcount)
                 values (?,?,?) """
       cur = self.conn.cursor()
-      cur.execute(sql, (dt_str, uniqueperson, count))
+      cur.execute(sql, (dt_str, uniqueperson, stationcount))
       self.conn.commit()
 
    def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:  # type: ignore
@@ -59,7 +59,7 @@ class Node(AbstractNode):
       """
 
       uniqueperson = inputs["obj_attrs"]
-      count = inputs["count"]
-      self.update_db(uniqueperson, count)
+      stationcount = inputs["count"]
+      self.update_db(uniqueperson, stationcount)
 
       return {}
