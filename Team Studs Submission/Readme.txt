75a11174d8d53a593e5880527dda68bfff1aaec2
trollorder <73745120+trollorder@users.noreply.github.com>
2023-02-04 18:44:22 +0800

    Cleaned Up

---
+++ Team Studs Submission/Readme.txt
@@ -0,0 +1,20 @@
+File Information
+
+Database Items:
+stationcount.db
+traincount.db
+
+Program scripts to be run in parrallel
+Stationcountdetector.py
+Traincountdetector.py
+SMARTQSystem Visualiser.py
+
+Custom nodes
+traincountsqlite
+stationcountsqlite
+
+LED Component Script
+LEDInstructor.py
+
+Testing Tools
+SQLDBTester
--- Team Studs Submission/SQLDBTester.py
+++ Team Studs Submission/SQLDBTester.py
@@ -1,48 +1,33 @@
 import sqlite3
 from random import randint
+import datetime
+import time
 class datainserter():
 
-   def __init__(self, dbpath : dict) -> None:
-      try:
-         # try to establish connection to database,
-         # will create DB_FILE if it does not exist
-         self.conn = sqlite3.connect(DB_FILE)
-         self.logger.info(f"Connected to {DB_FILE}")
-         sql = """ CREATE TABLE IF NOT EXISTS stationcounttable (
-                        datetime text,
-                        uniqueperson text,
-                        stationcount text
-                   ); """
-         cur = self.conn.cursor()
-         cur.execute(sql)
-      except sqlite3.Error as e:
-         self.logger.info(f"SQL Error: {e}")
+   def __init__(self, stationdbpath:str,traindbpath:str) -> None:
+      # try to establish connection to database,
+      # will create DB_FILE if it does not exist
+      self.stationcursor = sqlite3.connect(stationdbpath)
+      self.traincursor=sqlite3.connect(traindbpath)
 
-   def update_db(self, obj_attrs: str, stationcount: int) -> None:
+   def update_dbwithfalsevalues(self) -> None:
       """Helper function to save current time stamp, hand direction and
       wave count into DB wavetable.
       """
-      uniqueperson = str(obj_attrs) #manual coversion due to inability to rename
-      now = datetime.now()
+      now = datetime.datetime.now()
       dt_str = f"{now:%Y-%m-%d %H:%M:%S}"
       sql = """ INSERT INTO stationcounttable(datetime,uniqueperson,stationcount)
                 values (?,?,?) """
-      cur = self.conn.cursor()
-      cur.execute(sql, (dt_str, uniqueperson, stationcount))
-      self.conn.commit()
+      self.stationcursor.execute(sql, (dt_str, "Artifical Data", randint(1,20)))
+      self.stationcursor.commit()
+      sql2 = """ INSERT INTO traincounttable(datetime,uniqueperson,traincount)
+                values (?,?,?) """
+      self.traincursor.execute(sql2, (dt_str, "Artifical Data", randint(1,20)))
+      self.traincursor.commit()
+      
+newdbupdator = datainserter("stationcount.db","traincount.db")
 
-   def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:  # type: ignore
-      """Node to output hand wave data into sqlite database.
-
-      Args:
-            inputs (dict): Dictionary with keys "hand_direction", "num_waves"
-
-      Returns:
-            outputs (dict): Empty dictionary
-      """
-
-      uniqueperson = inputs["obj_attrs"]
-      stationcount = inputs["count"]
-      self.update_db(uniqueperson, stationcount)
-
-      return {}
+for x in range(1000):
+   time.sleep(1)
+   print("Updated")
+   newdbupdator.update_dbwithfalsevalues()
